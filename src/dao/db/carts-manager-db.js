4bab705cca642b7a08796e44901d24e941a1f6b2
jesgodoy <jesicagodoy7@gmail.com>
2024-08-23 20:09:22 -0300

    carts usando mongoose, mongo Db y populate

--- src/dao/db/carts-manager-db.js
+++ src/dao/db/carts-manager-db.js
@@ -1,89 +1,81 @@
-import fs from "fs";
+import CartModel from "../models/cart.model.js";
 
-class ProductManager {
-    constructor(path) {
-        this.products = [];
-        this.id = 1;
-        this.path = path;
-    }
-
-    
-    async addProduct({ title, category, description, price, urlImage, code, stock, thumbnails = [] }) {
-        const arrayProducts = await this.readFile();
-
-        if (!title || !category || !description || !price || !code || !stock) {
-            throw new Error('Todos los campos deben ser completados para agregar el producto.');
-        }
-        if (arrayProducts.some(product => product.code === code)) {
-            throw new Error("El código ya existe en otro producto. Por favor, ingrese un código diferente.");
-        }
-
-        const newId = arrayProducts.length > 0 ? arrayProducts[arrayProducts.length - 1].id + 1 : 1;
-        const newProduct = {
-            id: newId,
-            title,
-            category,
-            description,
-            price,
-            urlImage,
-            code,
-            stock,
-            status: true,
-            thumbnails
-        };
-
-        arrayProducts.push(newProduct);
-        await this.saveFile(arrayProducts);
-        return newProduct
-    }
-
-    async getProducts() {
+class CartManager {
+    async addCart() {
         try {
-            const arrayProducts = await this.readFile();
-            return arrayProducts;
+            const newCarrito = new CartModel({ cart: [] });
+            await newCarrito.save();
+            return newCarrito;
         } catch (error) {
-            console.error("No se pudo leer el archivo de productos", error);
+            console.error("Error al crear un carrito de compras:", error);
+            throw error;
         }
     }
 
-    async getProductById(id) {
-        const arrayProducts = await this.readFile();
-        const product = arrayProducts.find(prod => prod.id === id);
-        return product || null;
-    }
-
-    async readFile() {
-        const file = await fs.promises.readFile(this.path, "utf-8");
-        return JSON.parse(file);
-    }
-
-    async saveFile(arrayProducts) {
-        await fs.promises.writeFile(this.path, JSON.stringify(arrayProducts, null, 2));
-    }
-
-    async updateProduct(id, updatedProduct) {
-        const arrayProducts = await this.readFile();
-        const index = arrayProducts.findIndex(item => item.id === id);
-
-        if (index !== -1) {
-            arrayProducts[index] = { ...arrayProducts[index], ...updatedProduct };
-            await this.saveFile(arrayProducts);
-        } else {
-            throw new Error("No se encontró el producto");
+    async getCartById(id) {
+        try {
+            const cart = await CartModel.findById(id)
+                .populate({
+                    path: 'cart.product',
+                    select: 'id'
+                });
+            return cart || null;
+        } catch (error) {
+            console.error("Error al obtener el carrito por id:", error);
+            throw error;
         }
     }
 
-    async deleteProduct(id) {
-        const arrayProducts = await this.readFile();
-        const index = arrayProducts.findIndex(product => product.id === id);
+    async addProductToCart(id, productId, quantity = 1) {
+        try {
+            const cart = await this.getCartById(id);
+            if (!cart) {
+                throw new Error("Carrito no encontrado"); // Maneja el caso en que el carrito no existe
+            }
 
-        if (index !== -1) {
-            arrayProducts.splice(index, 1);
-            await this.saveFile(arrayProducts);
-        } else {
-            throw new Error("No se encontró el producto");
+            const existingProduct = cart.cart.find(prod => prod.product._id.toString() === productId);
+
+            if (existingProduct) {
+                existingProduct.quantity += quantity;
+            } else {
+                cart.cart.push({ product: productId, quantity });
+            }
+
+            cart.markModified("cart");
+            await cart.save();
+            return cart;
+        } catch (error) {
+            console.error("Error al agregar un producto al carrito:", error);
+            throw error;
         }
     }
+
+    async getAllCarts() {
+        try {
+            return await CartModel.find()
+                .populate({
+                    path: 'cart.product',
+                    select: 'id title price code'
+                });
+        } catch (error) {
+            console.error("Error al obtener todos los carritos:", error);
+            throw error;
+        }
+    }
+
+    async deleteCartById(id) {
+        try {
+            const result = await CartModel.findByIdAndDelete(id);
+            if (!result) {
+                throw new Error("Carrito no encontrado");
+            }
+            return result;
+        } catch (error) {
+            console.error("Error al eliminar el carrito por id:", error);
+            throw error;
+        }
+    }
+
 }
 
-export default ProductManager;
+export default CartManager;
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,53 +1,69 @@
 import express from "express";
-import CartManager from "../dao/db/carts-manager-db.js"
+import CartManager from "../dao/db/carts-manager-db.js";
 
 const router = express.Router();
-const cartManager = new CartManager("./src/data/carts.json");
+const cartManager = new CartManager();
 
 router.get("/", async (request, response) => {
     const limit = parseInt(request.query.limit);
     try {
-        const carts = await cartManager.loadCarts();
+        const carts = await cartManager.getAllCarts();
         response.status(200).json(isNaN(limit) ? carts : carts.slice(0, limit));
     } catch (error) {
         response.status(500).json({ error: "Error interno del servidor" });
     }
 });
 
+router.post("/", async (request, response) => {
+    try {
+        const newCart = await cartManager.addCart();
+        response.status(201).json({
+            message: "Carrito agregado correctamente",
+            cart: newCart
+        });
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+
 router.get("/:cid", async (request, response) => {
-    let id = parseInt(request.params.cid);
+    let id = request.params.cid;
     try {
         const searchCart = await cartManager.getCartById(id);
-        response.status(searchCart ? 200 : 404).json(searchCart || { error: "¡Producto no encontrado!" });
+        response.status(searchCart ? 200 : 404).json(searchCart || { error: "¡Carrito no encontrado!" });
     } catch (error) {
         response.status(500).json({ error: "Error interno del servidor" });
     }
 });
 
-router.post("/", async (request, response) => {
-    const newCart = request.body;
-
+router.delete("/:cid", async (request, response) => {
+    const cartId = request.params.cid;
     try {
-        await cartManager.addCart(newCart);
-        response.status(201).json({ message: "Carrito agregado correctamente" });
+        const deletedCart = await cartManager.deleteCartById(cartId);
+        response.status(200).json({ message: "Carrito eliminado correctamente" });
     } catch (error) {
-        console.error(error);
         response.status(500).json({ error: "Error interno del servidor" });
     }
 });
 
 router.post("/:cid/product/:pid", async (req, res) => {
-    const cartId = parseInt(req.params.cid);
+    const cartId = req.params.cid;
     const productId = req.params.pid;
     const quantity = req.body.quantity || 1;
 
     try {
+
         const cartUpdated = await cartManager.addProductToCart(cartId, productId, quantity);
-        res.status(200).json(cartUpdated.products);
+        res.status(200).json(cartUpdated);
     } catch (error) {
         console.error(error);
         res.status(500).send("Error al ingresar un producto al carrito");
     }
 });
+
+
+
 
 export default router;
